8a4fe89c4184a15db01df89faef113616bb299ef
Jason Raimondi <jason@raimondi.us>
2022-09-08 23:43:12 -0400

    chore: restyle readme

--- bin/update-readme.mjs
+++ bin/update-readme.mjs
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const rootPath = path.resolve(__dirname, "../");
 
 async function packageData(patterns) {
   const result = [];
@@ -12,6 +13,7 @@ async function packageData(patterns) {
   const dirs = files.filter(r => !r.includes('_sample'));
   for (let dir of dirs) {
     const content = await fs.readFile(dir, 'utf8');
+    dir = dir.replace(rootPath, "");
     const packageJSON = JSON.parse(content);
     result.push({
       dir,
@@ -24,18 +26,18 @@ async function packageData(patterns) {
 }
 
 void async function () {
-  const packages = await packageData([
+  const packages = (await packageData([
     path.resolve(__dirname, '../packages/*/package.json'),
-  ]);
+  ])).sort();
 
   const readmePath = path.resolve(__dirname, '../README.md');
   const readme = await fs.readFile(readmePath, 'utf8');
 
-  const template = packages.map(p => `
-#### [\`${p.name}\`](https://github.com/jasonraimondi/${p.dir})
+  const template = packages.map(p => `### [📁](https://github.com/jasonraimondi/jmondi/tree/main${p.dir}) ${p.name}
 
-* ${p.description}
-`).join('\n');
+[![NPM Downloads](https://img.shields.io/npm/dt/${p.name}?label=npm+downloads&style=flat-square)](https://www.npmjs.com/package/${p.name})
+
+${p.description}`).join('\n');
 
   const regex = /INSERT_START([\s\S]*?)INSERT_END/;
   const result = readme.replace(regex, `INSERT_START)
